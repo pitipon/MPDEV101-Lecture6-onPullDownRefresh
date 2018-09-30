f52240bc379b4cb507c73431a93667c860aa29e8
Pitipon Pluemworasawat <pitipon@gmail.com>
2018-09-30 11:20:26 +0700

    done

---
+++ pages/index/index.js
@@ -0,0 +1,17 @@
+//index.js
+//获取应用实例
+const app = getApp()
+
+Page({
+  data: {
+   
+  },
+  
+  onLoad: function () {
+  
+  },
+
+  onPullDownRefresh: function () {
+    console.log("OnPullDownRefresh Active")
+  }
+})
